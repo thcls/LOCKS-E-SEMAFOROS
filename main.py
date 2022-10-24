a22622a54d12f84e12eb131ed52f75c70332a21f
Thiago Carlos <thiagocarlossilvapereira@gmail.com>
2022-10-24 09:03:52 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from platform import release
+import threading
+from random import randint
+import time
+
+semaforo = threading.Semaphore(3)
+
+
+def atendercliente(i):
+    semaforo.acquire(i)
+    tempo = randint(3,10)
+    time.sleep(tempo)
+    semaforo.release()
+    print("Nº {} terminou".format(i))
+list = []
+for i in range(0,30):
+    list.append(threading.Thread(target=atendercliente, args=(i,)))
+    list[i].start()
+
+for i in list:
+    i.join()
